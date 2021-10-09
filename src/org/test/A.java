7ffdea84cdd4a03b6a29e07fe67b70d0a97de84b
Santosh1125 <santoshkumarp1126@gmail.com>
2021-10-09 18:01:37 +0530

    uptted

--- src/org/test/A.java
+++ src/org/test/A.java
@@ -2,15 +2,25 @@ package org.test;
 
 public class A extends B{
 
+	
+	
 	public A() {
 		super(12);
 		System.out.println("Default Constructor");
+		
 	}
+	public void mass() {
+		System.out.println("Default Constructor");
+		
+	}
+	
 	
 	
 	public static void main(String[] args) {
 	
 		A a = new A();
+	a.mass();
+		
 	}
 	
 }
